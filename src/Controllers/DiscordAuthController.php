98eac1eaddebe4e42ff156a9b43627f39ebc14f9
David Sevilla Martín <dsevilla192@icloud.com>
2019-06-13 15:44:48 -0400

    Switch to fof/extend for Auth controller, fixes redirect urls in subfolders

--- src/Controllers/DiscordAuthController.php
+++ src/Controllers/DiscordAuthController.php
@@ -11,91 +11,68 @@
 
 namespace FoF\AuthDiscord\Controllers;
 
-use Exception;
 use Flarum\Forum\Auth\Registration;
-use Flarum\Forum\Auth\ResponseFactory;
-use Flarum\Settings\SettingsRepositoryInterface;
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
-use Psr\Http\Server\RequestHandlerInterface;
+use League\OAuth2\Client\Provider\AbstractProvider;
+use FoF\Extend\Controllers\AbstractOAuthController;
 use Wohali\OAuth2\Client\Provider\Discord;
 use Wohali\OAuth2\Client\Provider\DiscordResourceOwner;
-use Zend\Diactoros\Response\RedirectResponse;
 
-class DiscordAuthController implements RequestHandlerInterface
+class DiscordAuthController extends AbstractOAuthController
 {
     /**
-     * @var ResponseFactory
+     * {@inheritdoc}
      */
-    protected $response;
-
-    /**
-     * @var SettingsRepositoryInterface
-     */
-    protected $settings;
-
-    /**
-     * @param ResponseFactory             $response
-     * @param SettingsRepositoryInterface $settings
-     */
-    public function __construct(ResponseFactory $response, SettingsRepositoryInterface $settings)
+    protected function getRouteName(): string
     {
-        $this->response = $response;
-        $this->settings = $settings;
+        return 'auth.discord';
     }
 
     /**
-     * @param ServerRequestInterface $request
-     *
-     * @throws Exception
-     *
-     * @return ResponseInterface
+     * {@inheritdoc}
      */
-    public function handle(ServerRequestInterface $request): ResponseInterface
+    protected function getProvider(string $redirectUri): AbstractProvider
     {
-        $redirectUri = (string) $request->getAttribute('originalUri', $request->getUri())->withQuery('');
-
-        $provider = new Discord([
+        return new Discord([
             'clientId'     => $this->settings->get('fof-auth-discord.client_id'),
             'clientSecret' => $this->settings->get('fof-auth-discord.client_secret'),
             'redirectUri'  => $redirectUri,
         ]);
+    }
 
-        $session = $request->getAttribute('session');
-        $queryParams = $request->getQueryParams();
+    /**
+     * {@inheritdoc}
+     */
+    protected function getProviderName(): string
+    {
+        return 'discord';
+    }
 
-        $code = array_get($queryParams, 'code');
+    /**
+     * {@inheritdoc}
+     */
+    protected function getAuthorizationUrlOptions(): array
+    {
+        return ['scope' => ['identify', 'email']];
+    }
 
-        if (!$code) {
-            $authUrl = $provider->getAuthorizationUrl(['scope' => ['identify', 'email']]);
-            $session->put('oauth2state', $provider->getState());
+    /**
+     * {@inheritdoc}
+     */
+    protected function getIdentifier($user): string
+    {
+        return $user->getId();
+    }
 
-            return new RedirectResponse($authUrl.'&display=popup');
-        }
-
-        $state = array_get($queryParams, 'state');
-
-        if (!$state || $state !== $session->get('oauth2state')) {
-            $session->remove('oauth2state');
-
-            throw new Exception('Invalid state');
-        }
-
-        $token = $provider->getAccessToken('authorization_code', compact('code'));
-
-        /** @var DiscordResourceOwner $user */
-        $user = $provider->getResourceOwner($token);
-
-        return $this->response->make(
-            'discord', $user->getId(),
-            function (Registration $registration) use ($user, $provider, $token) {
-                $registration
-                    ->provideTrustedEmail($user->getEmail())
-                    ->provideAvatar($this->getAvatar($user))
-                    ->suggestUsername($user->getUsername())
-                    ->setPayload($user->toArray());
-            }
-        );
+    /**
+     * {@inheritdoc}
+     */
+    protected function setSuggestions(Registration $registration, $user, string $token)
+    {
+        $registration
+            ->provideTrustedEmail($user->getEmail())
+            ->provideAvatar($this->getAvatar($user))
+            ->suggestUsername($user->getUsername())
+            ->setPayload($user->toArray());
     }
 
     private function getAvatar(DiscordResourceOwner $user)
